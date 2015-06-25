3daee8deb26bd27752d6c683864287a0301f9ca1
Steven Fackler <sfackler@gmail.com>
2015-06-24 23:20:14 -0700

    Release v0.6.0

---
+++ doc/v0.6.0/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;C, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='r2d2/config/struct.Config.html' title='r2d2::config::Config'>Config</a>&lt;C, E&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
