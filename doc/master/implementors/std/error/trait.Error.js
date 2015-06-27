66d52f1732e590da8098be17868eec8ea121ef02
Steven Fackler <sfackler@gmail.com>
2015-06-27 18:08:22 +0000

    rebuild pages at e09422ad2923a468361ab016f0ab2b1f48a47d9d

--- doc/master/implementors/std/error/trait.Error.js
+++ doc/master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['r2d2'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.InitializationError.html' title='r2d2::InitializationError'>InitializationError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.GetTimeout.html' title='r2d2::GetTimeout'>GetTimeout</a>",];
+implementors['r2d2'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.InitializationError.html' title='r2d2::InitializationError'>InitializationError</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='r2d2/struct.GetTimeout.html' title='r2d2::GetTimeout'>GetTimeout</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
