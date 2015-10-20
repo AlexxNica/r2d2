21577c7e366eaeb5ffc976e3f36fdc400018914a
Steven Fackler <sfackler@gmail.com>
2015-10-20 05:39:05 +0000

    rebuild pages at 6d563fd64f2a096678fe233d4fb72b147fec0da9

--- doc/master/implementors/core/ops/trait.DerefMut.js
+++ doc/master/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['r2d2'] = ["impl&lt;M&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='r2d2/struct.PooledConnection.html' title='r2d2::PooledConnection'>PooledConnection</a>&lt;M&gt;",];
+implementors['r2d2'] = ["impl&lt;M&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='r2d2/struct.PooledConnection.html' title='r2d2::PooledConnection'>PooledConnection</a>&lt;M&gt; <span class='where'>where M: <a class='trait' href='r2d2/trait.ManageConnection.html' title='r2d2::ManageConnection'>ManageConnection</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
