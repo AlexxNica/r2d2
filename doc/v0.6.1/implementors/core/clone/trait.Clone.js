5fd4b20095979cddc8cac9c4239c9fc13925e1e6
Steven Fackler <sfackler@gmail.com>
2015-10-19 22:58:15 -0700

    Release v0.6.1

---
+++ doc/v0.6.1/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='r2d2/struct.NopErrorHandler.html' title='r2d2::NopErrorHandler'>NopErrorHandler</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='r2d2/struct.LoggingErrorHandler.html' title='r2d2::LoggingErrorHandler'>LoggingErrorHandler</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='r2d2/struct.NopConnectionCustomizer.html' title='r2d2::NopConnectionCustomizer'>NopConnectionCustomizer</a>","impl&lt;M&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='r2d2/struct.Pool.html' title='r2d2::Pool'>Pool</a>&lt;M&gt; <span class='where'>where M: <a class='trait' href='r2d2/trait.ManageConnection.html' title='r2d2::ManageConnection'>ManageConnection</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.6.1/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['r2d2'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='r2d2/struct.NopErrorHandler.html' title='r2d2::NopErrorHandler'>NopErrorHandler</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='r2d2/struct.LoggingErrorHandler.html' title='r2d2::LoggingErrorHandler'>LoggingErrorHandler</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='r2d2/struct.NopConnectionCustomizer.html' title='r2d2::NopConnectionCustomizer'>NopConnectionCustomizer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
